dd97242d87815896f60663a34aeffc264e9c1840
syedtahamashhadi <syedtaha55@hotmail.com>
2019-10-11 14:01:03 +0500

    updated 10.js

--- Assignments/Assignment-3/10.js
+++ Assignments/Assignment-3/10.js
@@ -1,9 +1,6 @@
 var A = [20,53,78,4,91,12] ;
 
-function numberAs(a,b){
-    return a-b ;
-}
 
 document.getElementById('uArr').innerHTML = A ;
 
-document.getElementById('sArr').innerHTML = A.sort(numberAs) ;
+document.getElementById('sArr').innerHTML = A.sort( (a,b) => a-b  ) ;
